4d016f00bbc86346fed5d66709fd4731e83288a8
Dan Waltin <dan@waltin.se>
2020-06-19 22:24:54 +0200

    refactored

---
+++ Sources/GherkinSwift/GherkinFile.swift
@@ -0,0 +1,41 @@
+// ------------------------------------------------------------------------
+// Copyright 2020 Dan Waltin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+//
+//  GherkinFile.swift
+//  GherkinSwift
+//
+//  Created by Dan Waltin on 2020-06-19.
+//
+// ------------------------------------------------------------------------
+import Foundation
+
+public struct GherkinFile : Decodable {
+	public let gherkinDocument: GherkinDocument
+}
+
+public struct GherkinDocument : Equatable, Decodable {
+	public static func from(feature: Feature) -> GherkinDocument {
+		
+		return GherkinDocument(feature: PickledFeature(name: feature.name))
+	}
+
+	public let feature: PickledFeature
+}
+
+public struct PickledFeature : Equatable, Decodable {
+
+	public let name: String
+}
