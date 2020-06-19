30b371f002e334b0a1ad7e769f9c76093270ea13
Dan Waltin <dan@waltin.se>
2020-06-19 17:56:56 +0200

    Added Table and TableRow

---
+++ Sources/GherkinSwift/TableRow.swift
@@ -0,0 +1,41 @@
+// ------------------------------------------------------------------------
+// Copyright 2020 Dan Waltin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+//
+//  TableRow.swift
+//  SwiftSpec
+//
+//  Created by Dan Waltin on 2020-06-19.
+//
+// ------------------------------------------------------------------------
+
+import Foundation
+
+public struct TableRow : Equatable {
+	
+	public let cells: [String: String]
+	
+	internal init(cells: [String: String]) {
+		self.cells = cells
+	}
+	
+	public subscript(column: String) -> String {
+		return cells[column]!
+	}
+}
+
+public func ==(_ lhs: TableRow, _ rhs: TableRow) -> Bool {
+	return lhs.cells == rhs.cells
+}
