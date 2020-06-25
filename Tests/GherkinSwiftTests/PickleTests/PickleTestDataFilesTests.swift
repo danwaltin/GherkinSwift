ee1d2b15d4c1e1fbb3ed0701b38af2e8c9b8859b
Dan Waltin <dan@waltin.se>
2020-06-25 19:50:15 +0200

    adding tests for descriptions and comments

--- Tests/GherkinSwiftTests/PickleTests/PickleTestDataFilesTests.swift
+++ Tests/GherkinSwiftTests/PickleTests/PickleTestDataFilesTests.swift
@@ -27,7 +27,9 @@ import XCTest
 @available(OSX 10.15, *)
 class PickleTestDataFilesTests: XCTestCase {
 	let goodTests = [
+		"descriptions",
 		"empty",
+		"incomplete_feature_1",
 		"incomplete_feature_2",
 		"minimal"]
 
