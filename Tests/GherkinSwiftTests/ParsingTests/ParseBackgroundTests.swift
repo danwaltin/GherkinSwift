8eeec126a4df8ccb45bede566d681a200f898daf
Dan Waltin <dan@waltin.se>
2020-07-04 13:39:58 +0200

    Refactored assertions

--- Tests/GherkinSwiftTests/ParsingTests/ParseBackgroundTests.swift
+++ Tests/GherkinSwiftTests/ParsingTests/ParseBackgroundTests.swift
@@ -105,58 +105,61 @@ class ParseBackgroundTests: TestParseBase {
 	}
 
 	// MARK: - Givens, whens and thens
+	
+	private func assertBackground(_ file: StaticString, _ line: UInt, assertBackground: (Background) -> Void ) {
+		guard let actualBackground = actualFeature.background else {
+			XCTFail("No background found", file: file, line: line)
+			return
+		}
+		
+		assertBackground(actualBackground)
+	}
+
+	private func assertBackgroundStep(atIndex index: Int, _ file: StaticString, _ line: UInt, assertStep: (Step) -> Void ) {
+		assertBackground(file, line) {
+			if $0.steps.count <= index {
+				XCTFail("No step at index \(index)", file: file, line: line)
+				return
+			}
+			
+			let actualStep = $0.steps[index]
+			
+			assertStep(actualStep)
+		}
+	}
+
 	private func then_shouldReturnFeatureWithoutBackground(file: StaticString = #file, line: UInt = #line) {
 		XCTAssertNil(actualFeature.background, file: file, line: line)
 	}
 
 	private func then_shouldReturnBackgroundWithName(_ name: String,
 													 file: StaticString = #file, line: UInt = #line) {
-		guard let actualBackground = actualFeature.background else {
-			XCTFail("No background found", file: file, line: line)
-			return
+		assertBackground(file, line) {
+			XCTAssertEqual($0.name, name, file: file, line: line)
 		}
-
-		XCTAssertEqual(actualBackground.name, name, file: file, line: line)
 	}
 
 	private func then_shouldReturnBackgroundWithoutName(file: StaticString = #file, line: UInt = #line) {
-		guard let actualBackground = actualFeature.background else {
-			XCTFail("No background found", file: file, line: line)
-			return
+		assertBackground(file, line) {
+			XCTAssertEqual($0.name, "", file: file, line: line)
 		}
-		
-		XCTAssertEqual(actualBackground.name, "", file: file, line: line)
 	}
 
 	private func then_shouldReturnBackgroundWith(numberOfSteps expected: Int,
 												 file: StaticString = #file, line: UInt = #line) {
-		
-		guard let actualBackground = actualFeature.background else {
-			XCTFail("No background found", file: file, line: line)
-			return
+		assertBackground(file, line) {
+			XCTAssertEqual($0.steps.count, expected, file: file, line: line)
 		}
-		
-		XCTAssertEqual(actualBackground.steps.count, expected, file: file, line: line)
 	}
 	
 	private func then_shouldReturnBackgroundWithStep(_ stepType: StepType,
 													 _ text: String,
 													 atIndex index: Int = 0,
 													 file: StaticString = #file, line: UInt = #line) {
-		guard let actualBackground = actualFeature.background else {
-			XCTFail("No background found", file: file, line: line)
-			return
+		assertBackgroundStep(atIndex: index, file, line) {
+			XCTAssertEqual($0.type, stepType, file: file, line: line)
+			XCTAssertEqual($0.text, text, file: file, line: line)
 		}
-		
-		if actualBackground.steps.count <= index {
-			XCTFail("No step at index \(index)", file: file, line: line)
-			return
-		}
-		
-		let actual = actualBackground.steps[index]
-
-		XCTAssertEqual(actual.type, stepType, file: file, line: line)
-		XCTAssertEqual(actual.text, text, file: file, line: line)
 	}
 
 	private func then_shouldReturnBackgroundWithStep(atIndex index: Int = 0,
@@ -164,22 +167,12 @@ class ParseBackgroundTests: TestParseBase {
 													 _ text: String,
 													 _ table: Table,
 													 file: StaticString = #file, line: UInt = #line) {
-		guard let actualBackground = actualFeature.background else {
-			XCTFail("No background found", file: file, line: line)
-			return
-		}
-		
-		if actualBackground.steps.count <= index {
-			XCTFail("No step at index \(index)", file: file, line: line)
-			return
-		}
-		
-		let actual = actualBackground.steps[index]
+		assertBackgroundStep(atIndex: index, file, line) {
+			XCTAssertEqual($0.type, stepType, file: file, line: line)
+			XCTAssertEqual($0.text, text, file: file, line: line)
 
-		XCTAssertEqual(actual.type, stepType, file: file, line: line)
-		XCTAssertEqual(actual.text, text, file: file, line: line)
-		
-		let actualTable = actual.tableParameter!.withoutLocation()
-		XCTAssertEqual(actualTable, table, file: file, line: line)
+			let actualTable = $0.tableParameter!.withoutLocation()
+			XCTAssertEqual(actualTable, table, file: file, line: line)
+		}
 	}
 }
