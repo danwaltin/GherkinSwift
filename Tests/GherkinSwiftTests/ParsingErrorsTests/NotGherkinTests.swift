7caac539f744279483b716368a1b129ffd22892e
Dan Waltin <dan@waltin.se>
2020-07-17 15:13:58 +0200

    More tests

--- Tests/GherkinSwiftTests/ParsingErrorsTests/NotGherkinTests.swift
+++ Tests/GherkinSwiftTests/ParsingErrorsTests/NotGherkinTests.swift
@@ -31,20 +31,13 @@ class NotGherkinTests : TestErrorParseBase {
 		lorem ipsum
 		""")
 		
-		then_shouldReturnParseErrorWith(message:
-			"(1:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'lorem ipsum'")
-	}
-
-	func test_firstLineNotGherkin_location() {
-		when_parsingDocument(
-		"""
-		lorem ipsum
-		""")
+		then_shouldReturnParseErrorWith(
+			message: "(1:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'lorem ipsum'",
+			location: Location(column: 1, line: 1))
 		
-		then_shouldReturnParseErrorWith(location:
-			Location(column: 1, line: 1))
 	}
 
+	
 	func test_secondLineNotGherkin_message() {
 		when_parsingDocument(
 		"""
@@ -52,44 +45,109 @@ class NotGherkinTests : TestErrorParseBase {
 		rabarber
 		""")
 		
-		then_shouldReturnParseErrorWith(message:
-			"(2:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'rabarber'")
+		then_shouldReturnParseErrorWith(
+			message: "(2:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'rabarber'",
+			location: Location(column: 1, line: 2))
 	}
 
-	func test_secondLineNotGherkin_location() {
+	func test_invalidBetweenFeatureTagAndFeature() {
 		when_parsingDocument(
 		"""
-		
-		tomater
-		""")
-		
-		then_shouldReturnParseErrorWith(location:
-			Location(column: 1, line: 2))
-	}
-
-	func test_threeNonGherkinsBeforeFeature() {
-		when_parsingDocument(
-		"""
-		
-		rabarber
-
-		tomater
 		@featureTag
-		morötter
+
+		between
+
 		Feature: blubb
 		""")
 		
-		then_shouldReturnParseErrorWith(messages: [
-			"(2:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'rabarber'",
-			"(4:1): expected: #EOF, #Language, #TagLine, #FeatureLine, #Comment, #Empty, got 'tomater'",
-			"(6:1): expected: #TagLine, #FeatureLine, #Comment, #Empty, got 'morötter'"
-		])
+		then_shouldReturnParseErrorWith(message:
+			"(3:1): expected: #TagLine, #FeatureLine, #Comment, #Empty, got 'between'",
+			location: Location(column: 1, line: 3))
+	}
 
-		then_shouldReturnParseErrorWith(locations: [
-			Location(column: 1, line: 2),
-			Location(column: 1, line: 4),
-			Location(column: 1, line: 6)
-		])
+	func test_invalidBetweenScenarioTagAndScenario() {
+		when_parsingDocument(
+		"""
+		Feature: blubb
+		@scenarioTag
+		
+		not gherkin
+		Scenario: blabb
+		""")
+		
+		then_shouldReturnParseErrorWith(message:
+			"(4:1): expected: #TagLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'not gherkin'",
+			location: Location(column: 1, line: 4))
+	}
+
+	func test_invalidAfterScenarioStep() {
+		when_parsingDocument(
+		"""
+		Feature: blubb
+		Scenario: blabb
+		   Given something
+
+		not gherkin
+		""")
+		
+		then_shouldReturnParseErrorWith(message:
+			"(5:1): expected: #TableRow, #DocStringSeparator, #StepLine, #TagLine, #ExamplesLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'not gherkin'",
+			location: Location(column: 1, line: 5))
+	}
+
+	func test_invalidAfterBackgroundStep() {
+		when_parsingDocument(
+		"""
+		Feature: blubb
+		Background:
+			Given stuff
+
+		gherkin, not so much
+
+		Scenario: blabb
+		   Given something
+
+		""")
+		
+		then_shouldReturnParseErrorWith(message:
+			"(5:1): expected: #TableRow, #DocStringSeparator, #StepLine, #TagLine, #ExamplesLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'gherkin, not so much'",
+			location: Location(column: 1, line: 5))
+	}
+
+	func test_invalidBetweenExamplesTagAndExamples() {
+		when_parsingDocument(
+		"""
+		Feature: f
+		Scenario Outline: s
+		   Given the <thing> becomes visible
+		   
+		   @examplesTag
+		   nope, not gherkin
+		   Examples:
+		      | thing |
+		      | sun   |
+		""")
+		
+		then_shouldReturnParseErrorWith(message:
+			"(6:1): expected: #TagLine, #ExamplesLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'nope, not gherkin'",
+			location: Location(column: 1, line: 6))
+	}
+
+	func test_invalidInScenarioStepTable() {
+		when_parsingDocument(
+		"""
+		Feature: f
+		Scenario: s
+		   Given the following customers
+		      | Customer     |
+		      | Ada Lovelace |
+		      we expected a table row here!
+		      | Alan Turing  |
+		""")
+		
+		then_shouldReturnParseErrorWith(message:
+			"(6:1): expected: #TableRow, #StepLine, #TagLine, #ExamplesLine, #ScenarioLine, #RuleLine, #Comment, #Empty, got 'we expected a table row here!'",
+			location: Location(column: 1, line: 6))
 	}
 
 	func test_severalNonGherkinLines() {
@@ -145,7 +203,15 @@ class NotGherkinTests : TestErrorParseBase {
 		}
 	}
 	
-	private func then_shouldReturnParseErrorWith(message: String, file: StaticString = #file, line: UInt = #line) {
+	private func then_shouldReturnParseErrorWith(message: String,
+												 location: Location,
+												 file: StaticString = #file, line: UInt = #line) {
+		assert.parseError(withMessage: message, file, line)
+		assert.parseError(withLocation: location, file, line)
+	}
+
+	private func then_shouldReturnParseErrorWith(message: String,
+												 file: StaticString = #file, line: UInt = #line) {
 		assert.parseError(withMessage: message, file, line)
 	}
 
